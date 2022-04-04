068289ef20f930fcc8691143368b885d000d8630
Julien A. Raemy <julien.raemy@gmail.com>
2022-04-04 11:31:43 +0200

    restructure

--- form.py
+++ form.py
@@ -6,14 +6,31 @@ window = Tk()
 label = Label(text="Let's generate IIIF resources")
 label.pack()
 
+# ID (URL) of the IIIF resource
+
+idText=StringVar()
+idText.set("What is the URL of the IIIF resource?")
+idDir=Label(window, textvariable=idText, height=4)
+idDir.pack(side="left")
+
+directory=StringVar(None)
+id=Entry(window,textvariable=directory,width=50)
+id.insert(0, "https://")
+id.pack(side="left")
+
+# Manifest or Collection (context and type)
+
+v0=IntVar()
+v0.set(1)
+r1=Radiobutton(window, text="IIIF Manifest", variable=v0,value=1)
+r2=Radiobutton(window, text="IIIF Collection", variable=v0,value=2)
+r1.pack()
+r2.pack()
+
 var = StringVar()
 var.set("IIIF Manifest")
 data=("IIIF Manifest", "IIIF Collection")
 cb=Combobox(window, values=data)
-cb.place(x=20, y=60)
-
-## entry = Entry(width=40, bg="white", fg="black")
-## entry.pack()
-## entry.insert(0, "What is your name?")
+cb.place(x=50, y=150)
 
 window.mainloop()
