0d10a23efe8af7598b646f35d5b33ce7d068d272
Julien A. Raemy <julien.raemy@gmail.com>
2022-04-04 09:54:26 +0200

    just trying out tkinter

--- form.py
+++ form.py
@@ -4,4 +4,9 @@ window = tk.Tk()
 label = tk.Label(text="Let's generate IIIF resources")
 label.pack()
 
+entry = tk.Entry(width=40, bg="white", fg="black")
+entry.pack()
+
+entry.insert(0, "What is your name?")
+
 window.mainloop()
